06aff9d6c80314b1570d06a867e52d9068c9b3f5
JangJidam <jidamine87593@gmail.com>
2023-08-29 14:22:22 +0900

    cpg를 json으로 변환

--- convertGraphsonToJson.py
+++
@@ -1,9 +0,0 @@
-# graphson 파일을 우리에게 필요한 json 형태로 바꾸기 위함 
-import json
-
-try:
-    with open("\graphsonFiles\export.json","r") as file:
-        data = json.load(file)
-        data
-except:
-    print("error")
