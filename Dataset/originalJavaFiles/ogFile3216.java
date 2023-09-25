f8c0c19ad22283aaafe1e2590add4711689bf68c
SCO-JM <wlalssp@gmail.com>
2023-09-25 12:48:46 +0900

    remove: originalJavaFiles 삭제

--- Dataset/originalJavaFiles/ogFile3216.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class a {
-	public static void main(String[] args){
-		Scanner br = new Scanner(System.in);
-		long n = br.nextLong();
-		System.out.println("25");
-	}
-}
--- Dataset/originalJavaFiles/ogFile3261.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class A630 {
-	public static void main(String[] args)
-	{
-		Scanner scan = new Scanner(System.in);
-		long n = scan.nextLong();
-		System.out.println(25);
-	}
-}
