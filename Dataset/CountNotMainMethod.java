36f4d0135de594b3957d570f2a115fb257014ade
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-15 17:09:56 +0900

    refactor: 클래스명 변경
    Related to: #5

--- Dataset/CountNotMainMethod.java
+++ Dataset/CountNotMainMethod.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
-public class CheckOverLoading {
+public class CountNotMainMethod {
 
     public static void main(String[] args) {
 
